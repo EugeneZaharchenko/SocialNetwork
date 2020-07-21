83f1c2d0257fb53016285ecd8114bed4137d4bad
Eugene <eugenezaharchenko@gmail.com>
2020-07-21 11:54:08 +0300

    forms and templates added

--- app.py
+++ app.py
@@ -1,12 +1,7 @@
-from flask import Flask
+from flask import Flask, render_template, flash, redirect, url_for, session, request, logging
+from config import SECRET_KEY
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = SECRET_KEY
 
-
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
-
-
-if __name__ == '__main__':
-    app.run()
+import views
--- config.py
+++ config.py
@@ -1,2 +1,4 @@
 
 DB_NAME = "network.db"
+
+SECRET_KEY = 'guess-what'
