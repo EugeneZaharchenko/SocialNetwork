c5ecfd6cd2a98be34cbf3fc7ba26fd1fc00fe69c
Eugene <eugenezaharchenko@gmail.com>
2020-07-22 13:15:17 +0300

    token generated. Login implemented

--- models.py
+++ models.py
@@ -24,3 +24,11 @@ class Post(db.Model):
     def __repr__(self):
         return '<Post {}>'.format(self.body)
 
+class Vote(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    likes = db.Column(db.Integer(), default=0)
+    dislikes = db.Column(db.Integer(), default=0)
+    post_id = db.Column(db.Integer, db.ForeignKey('post.id'))
+
+    def __repr__(self):
+        return '<Post {0} likes: {1}, dislikes: {2}}>'.format(self.post_id, self.likes, self.dislikes)
--- views.py
+++ views.py
@@ -1,19 +1,19 @@
-from app import app, db
-from functools import wraps
-from flask import jsonify, request, make_response, render_template, flash, redirect, url_for, session
-import uuid
-import jwt
 import datetime
+import uuid
+from functools import wraps
+
+import jwt
+from flask import jsonify, request, make_response, Response, session, abort
 from werkzeug.security import generate_password_hash, check_password_hash
 
-from forms import LoginForm, RegisterForm
-from models import User
+from app import app, db
+from models import User, Post
 
 
 def token_required(f):
     @wraps(f)
     def decorated(*args, **kwargs):
-        token = None
+        token = request.args.get('token')
 
         if 'x-access-token' in request.headers:
             token = request.headers['x-access-token']
@@ -34,18 +34,18 @@ def token_required(f):
 
 @app.route('/')
 def home():
-    return render_template('home.html')
+    status_code = Response(status=201)
+    return status_code
 
 
 @app.route('/signup', methods=['POST'])
-@token_required
-def create_user(current_user):
+# @token_required
+def create_user():
     data = request.get_json()
 
     name = data['name']
     email = data['email']
     password = generate_password_hash(data['password'], method='sha256')
-
 
     new_user = User(public_id=str(uuid.uuid4()), username=name, email=email, password=password)
 
@@ -53,6 +53,36 @@ def create_user(current_user):
     db.session.commit()
 
     return jsonify({'message': 'New user created!'})
+
+
+@app.route('/login')
+def login():
+    auth = request.authorization
+    print(str(auth))
+
+    if not auth or not auth.username or not auth.password:
+        return make_response('Could not verify', 401, {'WWW-Authenticate' : 'Basic realm="Login required!"'})
+
+    user = User.query.filter_by(username=auth.username).first()
+
+    if not user:
+        return make_response('No such user', 401, {'WWW-Authenticate' : 'Basic realm="Login required!"'})
+
+    if check_password_hash(user.password, auth.password):
+        token = jwt.encode({'public_id' : user.public_id, 'exp' : datetime.datetime.utcnow() + datetime.timedelta(minutes=30)}, app.config['SECRET_KEY'])
+
+        return jsonify({'token' : token.decode('UTF-8')})
+
+    return make_response('Could not verify', 401, {'WWW-Authenticate' : 'Basic realm="Login required!"'})
+
+
+@app.route('/logout')
+# @is_logged_in
+def logout():
+    session.clear()
+    # flash('You are now logged out', 'success')
+    # return redirect(url_for('login'))
+
 
 @app.route('/user/<public_id>', methods=['GET'])
 @token_required
@@ -70,41 +100,40 @@ def get_one_user(current_user, public_id):
 
     return jsonify({'user' : user_data})
 
-@app.route('/login')
-def login():
-    auth = request.authorization
 
-    if not auth or not auth.username or not auth.password:
-        return make_response('Could not verify', 401, {'WWW-Authenticate' : 'Basic realm="Login required!"'})
+@app.route('/user/<string:username>/add_post', methods=['POST'])
+@token_required
+def add_post(current_user, username):
+    data = request.get_json()
 
-    user = User.query.filter_by(username=auth.username).first()
+    body = data['body']
+    user = User.query.filter_by(username=username).first()
+    user_id = user.id
 
-    if not user:
-        return make_response('Could not verify', 401, {'WWW-Authenticate' : 'Basic realm="Login required!"'})
+    new_post = Post(body=body, user_id=user_id)
 
-    if check_password_hash(user.password, auth.password):
-        token = jwt.encode({'public_id' : user.public_id, 'exp' : datetime.datetime.utcnow() + datetime.timedelta(minutes=30)}, app.config['SECRET_KEY'])
+    db.session.add(new_post)
+    db.session.commit()
 
-        return jsonify({'token' : token.decode('UTF-8')})
-
-    return make_response('Could not verify', 401, {'WWW-Authenticate' : 'Basic realm="Login required!"'})
+    return jsonify({'message': 'New post of user {} created!'.format(username)})
 
 
-def is_logged_in(f):
-    @wraps(f)
-    def wrap(*args, **kwargs):
-        if 'logged_in' in session:
-            return f(*args, **kwargs)
-        else:
-            flash('Unauthorized, please login', 'danger')
-            return redirect(url_for('login'))
+@app.route('/like/<int:post_id>/<int:user_id>')
+# @login_required
+def like(post_id,user_id):
+    posts = Post.select().where(Post.id == post_id)
+    if posts.count() == 0:
+        abort(404)
 
-    return wrap
+    post = Post.select().where(Post.id == post_id).get()
 
 
-@app.route('/logout')
-@is_logged_in
-def logout():
-    session.clear()
-    # flash('You are now logged out', 'success')
-    # return redirect(url_for('login'))
+@app.route('/dislike/<int:post_id>/<int:user_id>')
+# @login_required
+def dislike(post_id,user_id):
+    posts = Post.select().where(Post.id == post_id)
+
+    if posts.count() == 0:
+        abort(404)
+
+    post = Post.select().where(Post.id == post_id).get()
